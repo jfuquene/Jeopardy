2c2fa1666c0d7aea4db1aeb7d9d5d95de97bd0e4
John Fuquene <jfuquene@email.wm.edu>
2021-08-25 11:41:06 -0400

    map through array from api

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-async function getCatergories(){
+async function getCategories(){
     let response = await fetch("https://jservice.io/api/categories?count=4&offset=30")
     let data = response.json()
     return data
 }
+
+getCategories().then(categories => {
+    console.log(categories)
+    categories.map(category => {
+     let div = document.createElement("div")
+       div.textContent = category.title
+       let board = document.getElementById("board")
+       board.append(div)
+
+    })
+})
+
